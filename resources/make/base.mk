ef27746738f3c69c36b42792b7611d49fae2460b
Duncan M. McGreggor <oubiwann@twistedmatrix.com>
2014-09-01 16:41:36 -0700

    Updated make include with fixes.

--- resources/make/base.mk
+++ resources/make/base.mk
@@ -7,19 +7,17 @@ LIB = $(PROJECT)
 DEPS = ./deps
 BIN_DIR = ./bin
 EXPM = $(BIN_DIR)/expm
-
 SOURCE_DIR = ./src
 OUT_DIR = ./ebin
 TEST_DIR = ./test
 TEST_OUT_DIR = ./.eunit
 SCRIPT_PATH=$(DEPS)/lfe/bin:.:./bin:"$(PATH)":/usr/local/bin
-ERL_LIBS=$(shell $(LFETOOL) info erllibs):.:..
-EMPTY =
-ifeq ($(shell which lfetool),$EMPTY)
-	LFETOOL=$(BIN_DIR)/lfetool
+ifeq ($(shell which lfetool),)
+LFETOOL=$(BIN_DIR)/lfetool
 else
-	LFETOOL=lfetool
+LFETOOL=lfetool
 endif
+ERL_LIBS=$(shell $(LFETOOL) info erllibs):.:..
 OS := $(shell uname -s)
 ifeq ($(OS),Linux)
         HOST=$(HOSTNAME)
@@ -31,67 +29,71 @@ endif
 $(BIN_DIR):
 	mkdir -p $(BIN_DIR)
 
-$(LFETOOL): $(BIN_DIR)
-	@[ -f $(LFETOOL) ] || \
+get-lfetool: $(BIN_DIR)
 	curl -L -o ./lfetool https://raw.github.com/lfe/lfetool/master/lfetool && \
 	chmod 755 ./lfetool && \
 	mv ./lfetool $(BIN_DIR)
 
 get-version:
-	@PATH=$(SCRIPT_PATH) lfetool info version
+	@PATH=$(SCRIPT_PATH) $(LFETOOL) info version
+	@echo "Erlang/OTP, LFE, & library versions:"
+	@ERL_LIBS=$(ERL_LIBS) PATH=$(SCRIPT_PATH) erl \
+	-eval "lfe_io:format(\"~p~n\",['loise-util':'get-versions'()])." \
+	-noshell -s erlang halt
 
 $(EXPM): $(BIN_DIR)
 	@[ -f $(EXPM) ] || \
-	PATH=$(SCRIPT_PATH) lfetool install expm $(BIN_DIR)
+	PATH=$(SCRIPT_PATH) $(LFETOOL) install expm $(BIN_DIR)
 
 get-deps:
 	@echo "Getting dependencies ..."
 	@which rebar.cmd >/dev/null 2>&1 && rebar.cmd get-deps || rebar get-deps
-	@PATH=$(SCRIPT_PATH) lfetool update deps
+	@PATH=$(SCRIPT_PATH) $(LFETOOL) update deps
 
 clean-ebin:
 	@echo "Cleaning ebin dir ..."
 	@rm -f $(OUT_DIR)/*.beam
 
 clean-eunit:
-	@PATH=$(SCRIPT_PATH) lfetool tests clean
+	@PATH=$(SCRIPT_PATH) $(LFETOOL) tests clean
 
 compile: get-deps clean-ebin
 	@echo "Compiling project code and dependencies ..."
-	@which rebar.cmd >/dev/null 2>&1 &&
-	ERL_LIBS=$(ERL_LIBS) rebar.cmd compile || \
-	ERL_LIBS=$(ERL_LIBS) rebar compile
+	@which rebar.cmd >/dev/null 2>&1 && \
+	PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) rebar.cmd compile || \
+	PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) rebar compile
 
 compile-no-deps: clean-ebin
 	@echo "Compiling only project code ..."
 	@which rebar.cmd >/dev/null 2>&1 && \
-	ERL_LIBS=$(ERL_LIBS) rebar.cmd compile skip_deps=true || \
-	ERL_LIBS=$(ERL_LIBS) rebar compile skip_deps=true
+	PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) \
+	rebar.cmd compile skip_deps=true || \
+	PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) rebar compile skip_deps=true
 
 compile-tests:
-	@PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) lfetool tests build
+	@PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) $(LFETOOL) tests build
 
 repl: compile
 	@which clear >/dev/null 2>&1 && clear || printf "\033c"
 	@echo "Starting shell ..."
-	@PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) lfetool repl
+	@PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) $(LFETOOL) repl
 
 repl-no-deps: compile-no-deps
 	@which clear >/dev/null 2>&1 && clear || printf "\033c"
 	@echo "Starting shell ..."
-	@PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) lfetool repl
+	@PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) $(LFETOOL) repl
 
 clean: clean-ebin clean-eunit
 	@which rebar.cmd >/dev/null 2>&1 && rebar.cmd clean || rebar clean
 
 check-unit-only:
-	@PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) lfetool tests unit
+	@PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) $(LFETOOL) tests unit
 
 check-integration-only:
-	@PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) lfetool tests integration
+	@PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) $(LFETOOL) tests integration
 
 check-system-only:
-	@PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) lfetool tests system
+	@PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) $(LFETOOL) tests system
 
 check-unit-with-deps: get-deps compile compile-tests check-unit-only
 check-unit: compile-no-deps check-unit-only
@@ -100,11 +102,9 @@ check-system: compile check-system-only
 check-all-with-deps: compile check-unit-only check-integration-only \
 	check-system-only
 check-all: get-deps compile-no-deps
-	@PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) lfetool tests all
+	@PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) $(LFETOOL) tests all
 
 check: check-unit-with-deps
-
-check-travis: $(LFETOOL) check
 
 push-all:
 	@echo "Pusing code to github ..."
@@ -115,7 +115,7 @@ push-all:
 
 install: compile
 	@echo "Installing lmug-yaws ..."
-	@PATH=$(SCRIPT_PATH) lfetool install lfe
+	@PATH=$(SCRIPT_PATH) $(LFETOOL) install lfe
 
 upload: $(EXPM) get-version
 	@echo "Preparing to upload lmug-yaws ..."

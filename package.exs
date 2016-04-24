4d7ecd1cdfa3858654daaf93efce27187b223c33
Duncan McGreggor <oubiwann@gmail.com>
2016-04-24 11:07:46 -0500

    Replaced old package.exs with lfe.config.
    Also bumped micro version.

--- package.exs
+++
@@ -1,7 +0,0 @@
-Expm.Package.new(name: "loise",
-                 description: "A Noise-Generator for LFE",
-                 version: "0.0.1",
-                 keywords: ["LFE", "Noise", "Perlin", "Simplex"],
-                 maintainers: [[name: "Duncan McGreggor",
-                                email: "oubiwann@cogitat.io"]],
-                 repositories: [[github: "lfe/loise"]])
